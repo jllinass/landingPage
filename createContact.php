fbf8dae26c7f4f7206c3d710562015c66160d250
Joan <jllinass@esliceu.net>
2024-06-03 09:59:04 +0200

    com fer inserts segurs

--- createContact.php
+++ createContact.php
@@ -1,4 +1,5 @@
 <?php
+include 'db.php';
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
     echo "CREANDO CONTACTO NUEVO <BR>";
@@ -6,10 +7,27 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     $telefono=$_POST['telefono'];
     $correo=$_POST['correo'];
     $mensaje=$_POST['mensaje'];
+    
     echo "NOMBRE: ".$nombre."<br>";
     echo "TELEFONO: ".$telefono."<br>";
     echo "CORREO: ".$correo."<br>";
     echo "MENSAJE: ".$mensaje."<br>";    
+    
+    $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES 
+    (?,?,?,?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss", $nombre, $telefono, $correo, $mensaje);
+    $result = $stmt->execute();
+
+    if($result){
+        $stmt->close();
+        $conn->close();
+        echo "CONTACTO CREADO CON ÉXITO";
+        header("Location: contacto.php?status=succes&msg=Mensaje Guardado!");
+    }else{
+        echo "ERROR AL CREAR EL CONTACTO";
+    }
+
 }else{
     echo "ERROR EN MÉTODO POST";
 }
