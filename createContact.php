d1883c820363ee25461675b794a5fd3b961d0544
Joan <jllinass@esliceu.net>
2024-06-03 08:31:03 +0200

    canvis

---
+++ createContact.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "CREANDO CONTACTO NUEVO <BR>";
--- index.php
+++ index.php
@@ -51,43 +51,7 @@
                 <p>Guías por si estás empezando.</p>
             </section>
             </div>homePage
-        <section>
-            <h2>Contacto</h2>
-            <form class="formulario" action="">
-                <fieldset>
-                    <legend>Cóntactame</legend>
-                    <div class="contenedor-campos">
-                    <div class="campo">
-                        <div>
-                            <label for="">Nombre</label>
-                            <input class="input-text" type="text" name="nombre" placeholder="Tu nombre">
-                        </div>
-                    </div>
-                    <div class="campo">
-                        <div>
-                            <label for="">Teléfono</label>
-                            <input class="input-text" type="tel" name="teléfono" placeholder="Tu teléfono">
-                        </div>
-                    </div>
-                    <div class="campo">
-                        <div>
-                            <label for="">Correo</label>
-                            <input class="input-text" type="email" name="correo" placeholder="Tu correo">
-                        </div>
-                    </div>
-                    <div class="campo">
-                        <div>
-                            <label for="">Mensaje</label>
-                            <textarea class="input-text" name="mensaje" id="" cols="30" rows="10"></textarea>
-                        </div>
-                    </div>
-                    <div>
-                        <input class="boton" type="submit" value="Enviar">
-                    </div>
-                </div><!--contenedor campor-->
-                </fieldset>
-            </form>
-        </section>
+       
     </main>
 <?php
     include 'footer.php';
